793e93835ffb6b6a4e960036687807665d2befcb
Xiaosong Chen <xiaosong0911@gmail.com>
2018-05-15 15:54:08 +0800

    sort in numerical order

--- mesh_sequence_controller.py
+++ mesh_sequence_controller.py
@@ -37,6 +37,25 @@ import os
 import glob
 from bpy.app.handlers import persistent
 
+import re
+
+def tryint(s):
+    try:
+        return int(s)
+    except:
+        return s
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+
+def sort_nicely(l):
+    """ Sort the given list in the way that humans expect.
+    """
+    l.sort(key=alphanum_key)
+
 #global variable for the MeshSequenceController
 MSC = None
 
@@ -166,6 +185,7 @@ class MeshSequenceController:
         numFrames = 0
         unsortedFiles = glob.glob(full_filepath)
         sortedFiles = sorted(unsortedFiles)
+        sort_nicely(sortedFiles)
         #for each file that matches the glob query:
         for file in sortedFiles:
             #import the mesh file
