fde307668426e28282c8e0def3615e7dcfc3ba3d
Justin <neverhood311@gmail.com>
2020-06-07 09:08:52 -0600

    add Travis script for automatic releases (#56)

---
+++ version.py
@@ -0,0 +1,4 @@
+# IMPORTANT: don't change the "currentScriptVerison" variable name or version format. The Travis script depends on it looking like this
+# (major, minor, revision, development)
+currentScriptVersion = (2, 1, 0, "alpha.10")
+legacyScriptVersion = (2, 0, 2, "legacy")
