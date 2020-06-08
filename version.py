cac93fdf957aef4e4e6ba455291ccdb90d493f33
Justin <neverhood311@gmail.com>
2020-06-08 16:55:53 -0600

    use absolute paths for everything (#69)
    * use absolute paths for everything
    
    * bump version

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # IMPORTANT: don't change the "currentScriptVerison" variable name or version format. The Travis script depends on it looking like this
 # (major, minor, revision, development)
-currentScriptVersion = (2, 1, 0, "alpha.10")
+currentScriptVersion = (2, 1, 0, "alpha.11")
 legacyScriptVersion = (2, 0, 2, "legacy")
