db521b7e5dba634069f68cec8e5c7e60152e6479
Justin <neverhood311@gmail.com>
2020-06-12 21:48:16 -0600

    Switch "cleanup: false" back to "skip_cleanup: true" (#72)

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # IMPORTANT: don't change the "currentScriptVerison" variable name or version format. The Travis script depends on it looking like this
 # (major, minor, revision, development)
-currentScriptVersion = (2, 1, 0, "alpha.12")
+currentScriptVersion = (2, 1, 0, "alpha.13")
 legacyScriptVersion = (2, 0, 2, "legacy")
