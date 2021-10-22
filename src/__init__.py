4ac238c03a10f5eb732ae189d45eab6da1f1d604
Justin <neverhood311@gmail.com>
2021-10-22 14:35:28 -0500

    Import only the first mesh in each file (#145)
    Delete all other objects

--- src/__init__.py
+++ src/__init__.py
@@ -23,9 +23,9 @@ from .panels import *
 
 bl_info = {
     "name": "Stop motion OBJ",
-    "description": "Import a sequence of OBJ (or STL or PLY) files and display them each as a single frame of animation. This add-on also supports the .STL and .PLY file formats.",
+    "description": "Import a sequence of OBJ (or STL or PLY or X3D) files and display them each as a single frame of animation. This add-on also supports the .STL, .PLY, and .X3D file formats.",
     "author": "Justin Jensen",
-    "version": (2, 2, 0, "alpha.11"),
+    "version": (2, 2, 0, "alpha.12"),
     "blender": (2, 83, 0),
     "location": "File > Import > Mesh Sequence",
     "warning": "",
