81e9362b3cb6ca48d09ce6859b4e286f982f3569
jjensen <justin.jensen@ansys.com>
2024-04-05 15:30:45 -0500

    final version bump for v2.2.0

--- src/version.py
+++ src/version.py
@@ -2,5 +2,5 @@
 # (major, minor, revision, development)
 # example dev version: (1, 2, 3, "beta.4")
 # example release version: (2, 3, 4)
-currentScriptVersion = (2, 2, 0, "beta.1")
+currentScriptVersion = (2, 2, 0)
 legacyScriptVersion = (2, 0, 2, "legacy")
